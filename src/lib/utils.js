08d4f4505f0053a983b34d76e0c58eaceb3bf1ee
ginxo <emingora@redhat.com>
2020-07-30 15:38:34 +0200

    BXMSPROD-896 utils.test added and utils.js refactored

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,18 +1,14 @@
 const parser = require('git-diff-parser')
 const fetch = require('node-fetch')
 
-async function getChangedFiles (context) {
-  const changedFiles = new Set()
-
-  const response = await fetch(context.payload.pull_request.diff_url)
-  const diff = parser(await response.text())
-  for (const commit of diff.commits) {
-    for (const file of commit.files) {
-      changedFiles.add(file.name.toString())
-    }
+async function getChangedFiles(context) {
+  const url = context.payload.pull_request.diff_url
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error(`[ERROR] fetching ${url}. Status ${response.status}. Description ${response.statusText}`)
   }
-
-  return changedFiles
+  const diff = parser(await response.text())
+  return Array.from(diff.commits.reduce((newSet, commit) => commit.files.reduce((newSetFiles, file) => newSetFiles.add(file.name.toString()), newSet), new Set()))
 }
 
 module.exports = {
