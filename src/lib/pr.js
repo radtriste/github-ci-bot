10c58e024a9a68cffb8042124999c86f95ca38eb
ginxo <emingora@redhat.com>
2020-07-30 13:41:03 +0200

    BXMSPROD-896 pr.test added and pr.js refactored

--- src/lib/pr.js
+++ src/lib/pr.js
@@ -1,14 +1,8 @@
-async function isFirstPR (context) {
-  const respone = await context.github.issues.listForRepo(context.repo({
+async function isFirstPR(context) {
+  return await context.github.issues.listForRepo(context.repo({
     state: 'all',
     creator: context.payload.pull_request.user.login
-  }))
-  const countPR = respone.data.filter(data => data.pull_request)
-  if (countPR.length === 1) {
-    return true
-  } else {
-    return false
-  }
+  })).data.find(data => data.pull_request) !== undefined
 }
 
 module.exports = {
