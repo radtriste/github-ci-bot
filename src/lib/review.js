87e9fb8b82f185830f65d42327de41d920924bda
ginxo <emingora@redhat.com>
2020-07-31 07:04:06 +0200

    BXMSPROD-896 review.test properly refactored

--- src/lib/review.js
+++ src/lib/review.js
@@ -2,28 +2,23 @@ const globToRegExp = require('glob-to-regexp')
 const { getChangedFiles } = require('./utils')
 
 async function askToReview(context) {
+  const reviewersInfo = await context.config('bot-files/reviewers.yml')
   context.github.pulls.createReviewRequest(
     context.issue({
-      reviewers: await getPossibleReviewers(context, await context.config('bot-files/reviewers.yml'))
+      reviewers: await getPossibleReviewers(context, reviewersInfo)
     })
   )
 }
 
-async function getPossibleReviewers(context, allReviewers) {
-  const pathReviewers = new Set()
+async function getPossibleReviewers(context, reviewersInfo) {
   const changedFiles = await getChangedFiles(context)
-  for (const index in allReviewers.review) {
-    for (const path of allReviewers.review[index].paths) {
-      const re = globToRegExp(path)
-      if (changedFiles.find(file => re.test(file))) {
-        allReviewers.review[index].reviewers.forEach(reviewer => {
-          pathReviewers.add(reviewer)
-        })
-      }
-    }
-  }
-  return allReviewers.default.concat(Array.from(pathReviewers))
+  const pathReviewersSet = reviewersInfo.review
+    .filter(reviewPath => reviewPath.paths.map(path => globToRegExp(path)).find(re => changedFiles.find(file => re.test(file))))
+    .flatMap(pathReview => pathReview.reviewers)
     .filter(reviewer => reviewer !== context.payload.pull_request.user.login)
+    .reduce((acc, reviewer) => acc.add(reviewer), new Set())
+
+  return reviewersInfo.default.concat(Array.from(pathReviewersSet))
 }
 
 module.exports = {
--- test/review.test.js
+++ test/review.test.js
@@ -2,10 +2,11 @@ const review = require('../src/lib/review')
 jest.mock('../src/lib/utils')
 const { getChangedFiles: getChangedFilesMock } = require('../src/lib/utils')
 
+const reviewers = { default: ['default1', 'default2'], review: [{ paths: ['test/**', 'cmd/**'], reviewers: ['rw-a', 'rw-b'] }, { paths: ['packg/**'], reviewers: ['rw-c', 'rw-d'] }, { paths: ['packg2/**'], reviewers: ['rw-e', 'login'] }] }
+
 test('askToReview default reviewers', async () => {
   // Arrange
-  const reviewers = { default: ['default1', 'default2'], review: [{ paths: ['test/**', 'cmd/**'], reviewers: ['rw-a', 'rw-b'] }, { paths: ['packg/**'], reviewers: ['rw-c', 'rw-d'] }] }
-  const context = { config: jest.fn(path => path === 'bot-files/reviewers.yml' ? reviewers : undefined), github: { pulls: { createReviewRequest: jest.fn(param => 'toreview') } }, issue: jest.fn(param => 'issue'), payload: { pull_request: { user: { login: 'login' } } } }
+  const context = createContext(reviewers)
   getChangedFilesMock.mockImplementationOnce(cntxt => cntxt === context ? ['otherfolder/file-a'] : undefined)
   // Act
   await review.askToReview(context)
@@ -18,8 +19,7 @@ test('askToReview default reviewers', async () => {
 
 test('askToReview reviewer one file existing in path but the other', async () => {
   // Arrange
-  const reviewers = { default: ['default1', 'default2'], review: [{ paths: ['test/**', 'cmd/**'], reviewers: ['rw-a', 'rw-b'] }, { paths: ['packg/**'], reviewers: ['rw-c', 'rw-d'] }] }
-  const context = { config: jest.fn(path => path === 'bot-files/reviewers.yml' ? reviewers : undefined), github: { pulls: { createReviewRequest: jest.fn(param => 'toreview') } }, issue: jest.fn(param => 'issue'), payload: { pull_request: { user: { login: 'login' } } } }
+  const context = createContext(reviewers)
   getChangedFilesMock.mockImplementationOnce(cntxt => cntxt === context ? ['test/file-a', 'filex'] : undefined)
   // Act
   await review.askToReview(context)
@@ -32,8 +32,7 @@ test('askToReview reviewer one file existing in path but the other', async () =>
 
 test('askToReview reviewer both files existing in path ', async () => {
   // Arrange
-  const reviewers = { default: ['default1', 'default2'], review: [{ paths: ['test/**', 'cmd/**'], reviewers: ['rw-a', 'rw-b'] }, { paths: ['packg/**'], reviewers: ['rw-c', 'rw-d'] }] }
-  const context = { config: jest.fn(path => path === 'bot-files/reviewers.yml' ? reviewers : undefined), github: { pulls: { createReviewRequest: jest.fn(param => 'toreview') } }, issue: jest.fn(param => 'issue'), payload: { pull_request: { user: { login: 'login' } } } }
+  const context = createContext(reviewers)
   getChangedFilesMock.mockImplementationOnce(cntxt => cntxt === context ? ['test/file-a', 'cmd/filex'] : undefined)
   // Act
   await review.askToReview(context)
@@ -46,8 +45,7 @@ test('askToReview reviewer both files existing in path ', async () => {
 
 test('askToReview reviewer both files existing in different paths', async () => {
   // Arrange
-  const reviewers = { default: ['default1', 'default2'], review: [{ paths: ['test/**', 'cmd/**'], reviewers: ['rw-a', 'rw-b'] }, { paths: ['packg/**'], reviewers: ['rw-c', 'rw-d'] }] }
-  const context = { config: jest.fn(path => path === 'bot-files/reviewers.yml' ? reviewers : undefined), github: { pulls: { createReviewRequest: jest.fn(param => 'toreview') } }, issue: jest.fn(param => 'issue'), payload: { pull_request: { user: { login: 'login' } } } }
+  const context = createContext(reviewers)
   getChangedFilesMock.mockImplementationOnce(cntxt => cntxt === context ? ['test/file-a', 'packg/filex'] : undefined)
   // Act
   await review.askToReview(context)
@@ -57,3 +55,20 @@ test('askToReview reviewer both files existing in different paths', async () => 
   expect(context.github.pulls.createReviewRequest.mock.calls.length).toBe(1)
   expect(context.github.pulls.createReviewRequest.mock.calls[0][0]).toStrictEqual('issue');
 })
+
+test('askToReview reviewer login', async () => {
+  // Arrange
+  const context = createContext(reviewers)
+  getChangedFilesMock.mockImplementationOnce(cntxt => cntxt === context ? ['packg2/filex'] : undefined)
+  // Act
+  await review.askToReview(context)
+  // Assert
+  expect(context.issue.mock.calls.length).toBe(1)
+  expect(context.issue.mock.calls[0][0]).toStrictEqual({ 'reviewers': ['default1', 'default2', 'rw-e'] })
+  expect(context.github.pulls.createReviewRequest.mock.calls.length).toBe(1)
+  expect(context.github.pulls.createReviewRequest.mock.calls[0][0]).toStrictEqual('issue');
+})
+
+function createContext(reviewers) {
+  return { config: jest.fn(path => path === 'bot-files/reviewers.yml' ? reviewers : undefined), github: { pulls: { createReviewRequest: jest.fn(param => 'toreview') } }, issue: jest.fn(param => 'issue'), payload: { pull_request: { user: { login: 'login' } } } }
+}
