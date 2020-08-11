6b408124689b262ea7053edd48543d87ff6ae115
Tarun Khandelwal <tarkhand@redhat.com>
2020-08-11 11:05:47 +0530

    remove pull_request.edited event and use return on every function call (#21)

--- bin/main.js
+++ bin/main.js
@@ -5,17 +5,17 @@ const { getDelegate } = require("../src/lib/main/delegator");
  */
 module.exports = app => {
   app.on("pull_request.ready_for_review", async context => {
-    getDelegate(context).pullRequestReadyForReview(context);
+    return getDelegate(context).pullRequestReadyForReview(context);
   });
   app.on("pull_request.opened", async context => {
-    getDelegate(context).pullRequestOpened(context);
+    return getDelegate(context).pullRequestOpened(context);
   });
 
-  app.on(["pull_request.edited", "pull_request.synchronize"], async context => {
-    getDelegate(context).pullRequestChanged(context);
+  app.on("pull_request.synchronize", async context => {
+    return getDelegate(context).pullRequestChanged(context);
   });
 
   app.on("pull_request.reopened", async context => {
-    getDelegate(context).pullRequestReopened(context);
+    return getDelegate(context).pullRequestReopened(context);
   });
 };
